d159f5f4130afbb7988ac0b8bedeb9e67ac45a10
= <=>
2026-04-01 09:32:48 +0100

    proteger les routes des articles avec auth

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('articles', ArticleController::class);
+Route::resource('articles', ArticleController::class)->middleware('auth');
 
 
 Route::get('/dashboard', function () {
